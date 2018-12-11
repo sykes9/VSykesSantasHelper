de133073f20a515bf54ab1faa3b03baf082b7363
Veronica Sykes <sykes9@illinois.edu>
2018-12-11 10:45:11 -0600

    12/11 final project

--- src/main/java/com/example/vsykes/santashelper/ResultsTwo.java
+++ src/main/java/com/example/vsykes/santashelper/ResultsTwo.java
@@ -14,14 +14,64 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static com.example.vsykes.santashelper.ScrollingActivity.art;
+import static com.example.vsykes.santashelper.ScrollingActivity.cooking;
+import static com.example.vsykes.santashelper.ScrollingActivity.electronics;
+import static com.example.vsykes.santashelper.ScrollingActivity.gardening;
+import static com.example.vsykes.santashelper.ScrollingActivity.makeup;
+import static com.example.vsykes.santashelper.ScrollingActivity.music;
+import static com.example.vsykes.santashelper.ScrollingActivity.shopping;
+import static com.example.vsykes.santashelper.ScrollingActivity.sport;
+import static com.example.vsykes.santashelper.ScrollingActivity.videoGames;
+
 public class ResultsTwo extends AsyncTask<Void,Void,Void> {
     String data = " ";
     String dataParsed = " ";
     String singleParsed = " ";
     @Override
     protected Void doInBackground(Void... voids) {
+        //done
+        if (!sport) {
+            newURL("https://api.myjson.com/bins/ffzh0");
+        }
+        //done
+        if (!music) {
+            newURL("https://api.myjson.com/bins/71ruc");
+        }
+        //done
+        if (!electronics) {
+            newURL("https://api.myjson.com/bins/d291w");
+        }
+        //done
+        if (!art) {
+            newURL("https://api.myjson.com/bins/dcyus");
+        }
+        //done
+        if (!cooking) {
+            newURL("https://api.myjson.com/bins/1gppxw");
+
+        }
+        //done
+        if (!makeup) {
+            newURL("https://api.myjson.com/bins/qr7ys");
+        }
+        //done
+        if (!shopping) {
+            newURL("https://api.myjson.com/bins/wneus");
+        }
+        //done
+        if (!gardening) {
+            newURL("https://api.myjson.com/bins/nuetg");
+        }
+        //done
+        if (!videoGames) {
+            newURL("https://api.myjson.com/bins/1hdapg");
+        }
+        return null;
+    }
+    private void newURL(String input) {
         try {
-            URL url = new URL("http://api.walmartlabs.com/v1/items/541357139?format=json&apiKey=8ebhjrfucd3vygsr9cyk55hs");
+            URL url = new URL(input);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -33,10 +83,18 @@ public class ResultsTwo extends AsyncTask<Void,Void,Void> {
             JSONArray JA = new JSONArray(data);
             for (int i = 0; i < JA.length(); i++) {
                 JSONObject JO = (JSONObject) JA.get(i);
-                singleParsed = "Name:" + JO.get("name") + "\n";
+                singleParsed = "Name:" + JO.get("name") + "\n" + "\n" +
+                                "URL:" + JO.get("url") + "\t" + "\n" + "\n" + "\n" +
+                                "Name:" + JO.get("name1")+ "\n" + "\n" +
+                                "URL:" + JO.get("url1") + "\t" + "\n" + "\n" + "\n" +
+                                "Name:" + JO.get("name2")+ "\n" + "\n" +
+                                "URL:" + JO.get("url2") + "\t" + "\n" + "\n" + "\n"+
+                                "Name:" + JO.get("name3")+ "\n" + "\n" +
+                                "URL:" + JO.get("url3") + "\t" + "\n" + "\n" + "\n" +
+                                "Name:" + JO.get("name4")+ "\n" + "\n" +
+                                "URL:" + JO.get("url4") + "\t" + "\n" + "\n" + "\n";
                 dataParsed = dataParsed + singleParsed + "\n";
             }
-            System.out.println("catch1");
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -45,8 +103,36 @@ public class ResultsTwo extends AsyncTask<Void,Void,Void> {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return null;
+
     }
+//    private void newMakeup (String input) {
+//        try {
+//            URL url = new URL(input);
+//            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//            InputStream inputStream = httpURLConnection.getInputStream();
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//            String line = "";
+//            while(line != null) {
+//                line = bufferedReader.readLine();
+//                data = data + line;
+//            }
+//            JSONArray JA = new JSONArray(data);
+//            for (int i = 0; i < JA.length(); i++) {
+//                JSONObject JO = (JSONObject) JA.get(i);
+//                singleParsed = "Name:" + JO.get("name") + "\n" + "\n" +
+//                        "URL:" + JO.get("url") + "\n";
+//                dataParsed = dataParsed + singleParsed + "\n";
+//            }
+//
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
     @Override
     protected void onPostExecute(Void aVoid) {
